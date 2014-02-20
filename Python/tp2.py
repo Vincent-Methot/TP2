33fdfc7a3b5018b60d4dc89e4dd9f80b245c09b8
Vincent Méthot <vincent.methot@gmail.com>
2014-02-20 08:39:28 -0500

    Ajout d'une fonction pour ouvrir des images dans un numpy array

--- Python/tp2.py
+++ Python/tp2.py
@@ -3,17 +3,24 @@
 # Fonctions du tp2 du cours IMN530 - H14 - Jérémie Fouquet et Vincent Méthot
 
 import numpy as np
+import nibabel as nib
+import Image
 
 def JointHist(I, J, bin):
 	"""Calcule l'histogramme conjoint de deux images de même taille (I et J)
 	en divisant leur intervalle de valeurs en 'bin' sous-intervalles"""
 
+	I = openImage(I)
+	J = openImage(J)
+
 
 def SSD(I, J):
-	"""Calcule la somme des différences au carré entre 2 images (I et J) de même taille"""
+	"""Calcule la somme des différences au carré entre 2 images (I et J)
+	de même taille"""
 
 def CR(I, J):
-	"""Calcule le coefficient de corrélation entre 2 images (I et J) de même taille"""
+	"""Calcule le coefficient de corrélation entre 2 images (I et J)
+	de même taille"""
 
 def IM(I, J):
 	"""Calcule l'information mutuelle entre 2 images de même taille"""
@@ -27,7 +34,8 @@ def trans_rigide(theta, omega, phi, p, q, r):
 	(p, q, r): 	vecteur de translation"""
 
 def similitude(s, theta, omega, phi, p, q, r):
-	"""Revoie la matrice de transformation rigide (+homothétie) en coordonnées homogènes
+	"""Revoie la matrice de transformation rigide (+homothétie)
+	en coordonnées homogènes
 	-------------------------------------------------------------------
 	s: 			rapport de l'homothétie
 	theta: 		angle de rotation autour de x
@@ -41,21 +49,37 @@ def translation(I, p, q):
 
 	La gestion de l'interpolation est effectuée par [module d'interpolation]"""
 
-def rec2dtrans(I):
+def rec2dtrans(I, J):
 	"""Recalage 2D minimisant la SSD et considérant uniquement les translations.
 	L'énergie SSD correspondant à chaque état est sauvegardée."""
 
 def rotation(I, theta):
-	"""Application d'une rotation d'angle 'theta' et de centre (0, 0) (coin supérieur gauche) à l'image 'I'"""
+	"""Application d'une rotation d'angle 'theta' et de centre (0, 0)
+	(coin supérieur gauche) à l'image 'I'"""
 
-def rec2drot(I):
+def rec2drot(I, J):
 	"""Recalage 2D minimisant la SSD et considérant uniquement les rotations.
 	L'énergie SSD correspondant à chaque état est sauvegardée."""
 
-def rec2dpasfixe(I):
+def rec2dpasfixe(I, J):
 	"""Recalage 2D minimisant la SSD par une descente de gradient.
 	Considère l'ensemble des transformations rigides."""
 
-def rec2doptimize(I):
+def rec2doptimize(I, J):
 	"""Recalage 2D minimisant la SSD par une descente de gradient optimisée.
 	Considère l'ensemble des transformations rigides."""
+
+
+
+def openImage(I):
+	"""Ouvre des images au format jpeg, png et NifTI et les retourne en numpy asarray"""
+
+	if I[-7:] == '.nii.gz' | I[-4:] == '.nii':
+		J = np.asarray(nib.load(I).get_data())
+	elif I[-4:] == '.jpg' | I[-4:] == '.png' | I[-5] == '.jpeg':
+		J = np.asarray(Image.open(I))
+	else:
+		print "Formats d'image acceptés: .nii, .nii.gz, .jpg, .png, .jpeg"
+
+	return J
+		
