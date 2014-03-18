b34eb95d6bb62e5333e5bdd7d7b94795611b4810
Vincent Méthot <vincent.methot@gmail.com>
2014-03-18 00:29:00 -0400

    Ajout du recalage linéaire flirt

--- ANTS/ants.sh
+++ ANTS/ants.sh
@@ -1,6 +1,10 @@
 #! /bin/bash
 # ANTS.sh
 
+# Estimation de la transformation non-linéaire
 ANTS 3 -m MI[t1.nii, flair.nii, 1, 32] -o flair_to_t1.nii.gz -i 30x20x10 -r Gauss[3,1] -t Elast[3]
-
+# Application de la transformation non-linéaire
 WarpImageMultiTransform 3 flair.nii flair_to_t1.nii.gz -R t1.nii flair_to_t1Warp.nii.gz flair_to_t1Affine.txt
+
+# Recalage linéaire
+flirt -dof 6 -cost mutualinfo -in flair.nii -out flair_lineaire.nii.gz -ref t1.nii 
